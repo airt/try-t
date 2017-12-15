d6264f9bcaef323e5683174b05ba0fe5b7598b07
airt <airtial@gmail.com>
2017-12-15 22:36:35 +0800

    style: fix tslint issues

--- src/try.ts
+++ src/try.ts
@@ -1,3 +1,5 @@
+// tslint:disable: no-use-before-declare
+
 export interface TryAsJson<A> {
   type: string
   success: boolean
@@ -9,7 +11,6 @@ export type Fn<A, B> = (x: A) => B
 export type Fn2<A, B, C> = (a: A) => (b: B) => C
 
 export default abstract class Try<A> implements Iterable<A> {
-
   abstract isFailure: boolean
 
   abstract isSuccess: boolean
@@ -69,27 +70,29 @@ export default abstract class Try<A> implements Iterable<A> {
   }
 
   static p = <A>(p: Promise<A>): Promise<Try<A>> =>
-    p.then(Try.success).catch(Try.failure)
+    p.then(Try.success).catch(e => Try.failure(e))
 
   // static join = <A>(t: Try<Try<A>>): Try<A> =>
   //   t.flatMap(x => x)
 
-  match = <B>(matchers: { success: Fn<A, B>, failure: Fn<Error, B> }): B =>
-    this.fold(matchers.failure, matchers.success)
+  match = <B>(matchers: { success: Fn<A, B>; failure: Fn<Error, B> }): B =>
+    this.fold(matchers.failure, matchers.success);
 
-  ;[Symbol.iterator] = (): Iterator<A> => this.toArray()[Symbol.iterator]()
-
+  [Symbol.iterator] = (): Iterator<A> => this.toArray()[Symbol.iterator]()
 }
 
 class Failure<A> extends Try<A> {
-
   isFailure = true
 
   isSuccess = false
 
-  constructor(private readonly error: Error) { super() }
+  constructor(private readonly error: Error) {
+    super()
+  }
 
-  get = (): A => { throw this.error }
+  get = (): A => {
+    throw this.error
+  }
 
   getOrElse = (other: () => A): A => other()
 
@@ -105,20 +108,21 @@ class Failure<A> extends Try<A> {
 
   flatMap = <B>(f: Fn<A, Try<B>>): Try<B> => this.asTryB<B>()
 
-  foreach = (f: Fn<A, void>): void => (void 0)
+  foreach = (f: Fn<A, void>): void => void 0
 
-  onFailure = (f: Fn<Error, void>): Try<A> => { f(this.error); return this }
+  onFailure = (f: Fn<Error, void>): Try<A> => {
+    f(this.error)
+    return this
+  }
 
   onSuccess = (f: Fn<A, void>): Try<A> => this
 
   transform = <B>(hs: Fn<A, Try<B>>, hf: Fn<Error, Try<B>>): Try<B> =>
     Try.m(() => hf(this.error))
 
-  recover = (f: Fn<Error, A>): Try<A> =>
-    Try.of(() => f(this.error))
+  recover = (f: Fn<Error, A>): Try<A> => Try.of(() => f(this.error))
 
-  recoverWith = (f: Fn<Error, Try<A>>): Try<A> =>
-    Try.m(() => f(this.error))
+  recoverWith = (f: Fn<Error, Try<A>>): Try<A> => Try.m(() => f(this.error))
 
   toArray = (): Array<A> => []
 
@@ -133,17 +137,17 @@ class Failure<A> extends Try<A> {
 
   toString = () => `Failure(${this.error.name}: ${this.error.message})`
 
-  private asTryB = <B>() => this as any as Try<B>
-
+  private asTryB = <B>() => (this as any) as Try<B>
 }
 
 class Success<A> extends Try<A> {
-
   isFailure = false
 
   isSuccess = true
 
-  constructor(private readonly value: A) { super() }
+  constructor(private readonly value: A) {
+    super()
+  }
 
   get = (): A => this.value
 
@@ -156,22 +160,31 @@ class Success<A> extends Try<A> {
   failed = (): Try<Error> => Try.failure(new Error('failed of success'))
 
   fold = <B>(hf: Fn<Error, B>, hs: Fn<A, B>): B => {
-    try { return hs(this.value) } catch (e) { return hf(e) }
+    try {
+      return hs(this.value)
+    } catch (e) {
+      return hf(e)
+    }
   }
 
   filter = (p: (x: A) => boolean): Try<A> =>
-    this.flatMap(x =>
-      p(x) ? this : Try.failure(new Error(`predicate does not hold for ${this.value}`)),
+    this.flatMap(
+      x =>
+        p(x)
+          ? this
+          : Try.failure(new Error(`predicate does not hold for ${this.value}`)),
     )
 
-  flatMap = <B>(f: Fn<A, Try<B>>): Try<B> =>
-    Try.m(() => f(this.value))
+  flatMap = <B>(f: Fn<A, Try<B>>): Try<B> => Try.m(() => f(this.value))
 
   foreach = (f: Fn<A, void>): void => f(this.value)
 
   onFailure = (f: Fn<Error, void>): Try<A> => this
 
-  onSuccess = (f: Fn<A, void>): Try<A> => { f(this.value); return this }
+  onSuccess = (f: Fn<A, void>): Try<A> => {
+    f(this.value)
+    return this
+  }
 
   transform = <B>(hs: Fn<A, Try<B>>, hf: Fn<Error, Try<B>>): Try<B> =>
     this.flatMap(hs)
@@ -189,10 +202,6 @@ class Success<A> extends Try<A> {
   })
 
   toString = () => `Success(${this.value})`
-
 }
 
-export {
-  Failure as _Failure,
-  Success as _Success,
-}
+export { Failure as _Failure, Success as _Success }
